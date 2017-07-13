96c31fbb1afef0ae0490f2ba2dc13f3adbdf56e0
emillevic <emillesampaio2013@gmail.com>
2017-07-13 00:52:43 -0300

    added pop ups with points loss description's

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $("header").css({'background-image':'url(images/ecomp/logo.png)', 'transition': 'background-image 10s',
                         'background-repeat':'no-repeat', 'background-position' : 'center', 'background-size': '300px'
                         
-                        }).fadeTo('slow', 1);
+                        }).show(1000);
     });
 
     var $doc = $('html, body');
@@ -14,4 +14,38 @@ $(document).ready(function(){
         }, 500);
         return false;
     });
+
+        $("a[rel=modal]").click( function(ev){
+        ev.preventDefault();
+ 
+        var id = $(this).attr("href");
+ 
+
+
+    var alturaTela = $(document).height();
+    var larguraTela = $(window).width();
+
+    $('#mascara').css({'width':larguraTela,'height':alturaTela});
+
+        $('#mascara').fadeIn(1000); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /6) - ( $(id).width() / 6 );
+        var top = ($(window).height() / 6) - ( $(id).height() / 6);
+     
+        $(id).css({'top':top,'left':left});
+        $(id).show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $(".window").hide();
+    });
+ 
+    $('.fechar').click(function(ev){
+        ev.preventDefault();
+        $("#mascara").hide();
+        $(".window").hide();
+    });
+
 });
