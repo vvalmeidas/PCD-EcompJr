d2ec363aec55c58c9b1c334688d619516342ef1a
vvalmeida <vvalmeida96@gmail.com>
2017-07-13 23:15:52 -0300

    new icon style (fading and border)

--- javascript.js
+++ javascript.js
@@ -1,51 +1,61 @@
-$(document).ready(function(){
-    $("h1").mouseenter(function(){
+$(document).ready(function () {
+    $("h1").mouseenter(function () {
 
-        $("header").css({'background-image':'url(images/ecomp/logo.png)', 'transition': 'background-image 10s',
-                        'background-repeat':'no-repeat', 'background-position' : 'center', 'background-size': '300px'
-                        
-                        }).show(1000);
+        $("header").css({
+            'background-image': 'url(images/ecomp/logo.png)', 'transition': 'background-image 10s',
+            'background-repeat': 'no-repeat', 'background-position': 'center', 'background-size': '300px'
+
+        }).show(1000);
     });
 
     var $doc = $('html, body');
-    $('.scrollSuave').click(function() {
+    $('.scrollSuave').click(function () {
         $doc.animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
         return false;
     });
 
-        $("a[rel=modal]").click( function(ev){
+    $("a[rel=modal]").click(function (ev) {
         ev.preventDefault();
- 
+
         var id = $(this).attr("href");
- 
 
 
-    var alturaTela = $(document).height();
-    var larguraTela = $(window).width();
 
-    $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
 
-        $('#mascara').fadeIn(1000); 
-        $('#mascara').fadeTo("slow",0.8);
- 
-        var left = ($(window).width() /6) - ( $(id).width() / 6 );
-        var top = ($(window).height() / 6) - ( $(id).height() / 6);
-     
-        $(id).css({'top':top,'left':left});
-        $(id).show();   
+        $('#mascara').css({ 'width': larguraTela, 'height': alturaTela });
+
+        $('#mascara').fadeIn(1000);
+        $('#mascara').fadeTo("slow", 0.8);
+
+        var left = ($(window).width() / 6) - ($(id).width() / 6);
+        var top = ($(window).height() / 6) - ($(id).height() / 6);
+
+        $(id).css({ 'top': top, 'left': left });
+        $(id).show();
     });
- 
-    $("#mascara").click( function(){
+
+    $("#mascara").click(function () {
         $(this).hide();
         $(".window").hide();
     });
- 
-    $('.fechar').click(function(ev){
+
+    $('.fechar').click(function (ev) {
         ev.preventDefault();
         $("#mascara").hide();
         $(".window").hide();
     });
+
+    // handle the mouseenter functionality
+    $(".img").mouseenter(function () {
+        $(this).addClass("hover");
+    })
+        // handle the mouseleave functionality
+        .mouseleave(function () {
+            $(this).removeClass("hover");
+        });
 
 });
